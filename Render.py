39d06f076b209351d2c90ed1f184387e58ab6ae2
ALEXANDR <yakolyagin@yandex.ru>
2021-01-06 15:05:27 +0300

    append sprites

--- Render.py
+++ Render.py
@@ -5,30 +5,32 @@ from Floor import wall_coords
 
 
 class Render:
-    def __init__(self, screen, screen_map):
+    def __init__(self, screen, screen_map, screen2):
         self.screen = screen
         self.mini_map_screen = screen_map
+        self.screen2 = screen2
         self.font = pygame.font.SysFont('Arial', 36, bold=True)
-        self.texturs = {'1': pygame.image.load('Data\Texture\stone1.jpg').convert(),
+        self.texturs = {'1': pygame.image.load('Data\Texture\DungeonWall.jpg').convert(),
                         '2': pygame.image.load('Data\Texture\stone.jpg').convert(),
-                        '3': pygame.image.load('Data\Texture\Ruby.jpg').convert(),
-                        '4': pygame.image.load('Data\Texture\CaveWall.jpg').convert()}
+                        '3': pygame.image.load('Data\Texture\stone2.jpg').convert(),
+                        '4': pygame.image.load('Data\Texture\metall.jpg').convert()}
 
     def background(self):
         pygame.draw.rect(self.screen, COLOR_DARKSLATEGRAY, (0, 0, SIZE[0], SIZE[1] // 2))
         pygame.draw.rect(self.screen, COLOR_DARKGRAY, (0, SIZE[1] // 2, SIZE[0], SIZE[1] // 2))
 
-    def world(self, player_pos, player_angle):
-        block_v, block_h = raycast(self.screen, player_pos, player_angle, self.texturs)
-        return block_v, block_h
+    def world(self, world_object):
+        for obj in sorted(world_object, key=lambda n: n[0], reverse=True):
+            if obj[0]:
+                _, object, object_pos = obj
+                self.screen.blit(object, object_pos)
 
     def mini_map(self, player):
         self.mini_map_screen.fill(COLOR_BLACK)
         map_x, map_y = player.player_pos()[0] // 5, player.player_pos()[1] // 5
-        pygame.draw.line(self.mini_map_screen, COLOR_BLUE, (map_x + 1, map_y + 1), (map_x + 12 * math.cos(player.angle),
-                                                                                    map_y + 12 * math.sin(
-                                                                                        player.angle)), 2)
-        pygame.draw.rect(self.mini_map_screen, COLOR_RED, (int(map_x), int(map_y), 5, 5))
+        pygame.draw.line(self.mini_map_screen, COLOR_BLUE, (map_x+1, map_y+1), (map_x + 12 * math.cos(player.angle),
+                                                                            map_y + 12 * math.sin(player.angle)), 2)
+        pygame.draw.rect(self.mini_map_screen, COLOR_RED, (int(map_x), int(map_y),5,5))
         COLOR = COLOR_WHITE
         for x, y in wall_coords:
             if wall_texture[(x, y)] == '1':
@@ -36,8 +38,8 @@ class Render:
             elif wall_texture[(x, y)] == '2':
                 COLOR = (255, 255, 20)
             elif wall_texture[(x, y)] == '3':
-                COLOR = (255, 0, 0)
+                COLOR = (30, 30, 60)
             elif wall_texture[(x, y)] == '4':
                 COLOR = (40, 40, 45)
             pygame.draw.rect(self.mini_map_screen, COLOR, (x // 5, y // 5, MAP_WALL, MAP_WALL))
-        self.screen.blit(self.mini_map_screen, MAP_POS)
+        self.screen2.blit(self.mini_map_screen, MAP_POS)
--- main.py
+++ main.py
@@ -5,37 +5,35 @@ from Player import *
 from Floor import *
 from RayCasting import *
 from Render import *
-
+from Sprite_render import *
 mixer = pygame.mixer
 mixer.init()
 mixer.music.load('Data/Music/music1.mp3')
 mixer.music.set_volume(0.05)
 mixer.music.play()
-counter = 0
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode(SIZE)
+    screen2 = pygame.display.set_mode(SIZE)
+    screen = pygame.Surface(SIZE)
     running = True
+    sprites = Sprites()
     clock = pygame.time.Clock()
     player = Player()
     screen_map = pygame.Surface((SIZE[0] // 5, SIZE[1] // 5))
-    render = Render(screen, screen_map)
+    render = Render(screen, screen_map, screen2)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_q:
-                    counter = player.destroy_block(block,counter)
-                if ruby_block not in wall_coords:
-                    print(f'Вы победили,потратив на это {counter} действий')
-                    running=False
-
         player.movement()
+
         render.background()
-        block_v, block_h = render.world(player.player_pos(), player.angle)
-        block = player.block_check(block_v, block_h)
+        walls = raycast(player, render.texturs)
+        render.world(walls + [obj.object_locate(player, walls)  for obj in sprites.list_sprites])
         render.mini_map(player)
         clock.tick(600)  # Установка ограничения FPS
         pygame.display.flip()
+        screen2.fill(COLOR_DARKSLATEGRAY)
+        screen2.blit(screen, (0, 0 + float(player.z_player())))
+        print(clock.get_fps())
     pygame.quit()
